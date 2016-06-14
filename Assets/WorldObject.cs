97755914b8f2535b93f47ab27c32200c02865ad4
Shaun Landy <Shaun2.Landy@live.uwe.ac.uk>
2016-06-14 15:12:14 +0100

    Tile and World Object
    Every physical object in the world should be a world object
    Tiles represent what world objects are on top of

---
+++ Assets/WorldObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldObject : MonoBehaviour
+{
+    //The tiles the object is associated with
+    private Tile[] tile;
+
+    private void Awake()
+    {
+
+    }
+
+    //The behavior of an object when something tries to interact with it
+    public virtual void Interaction(WorldObject _obj)
+    {
+
+    }
+
+
+}
