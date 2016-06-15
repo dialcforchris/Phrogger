9831e898a0b1c869eef21ca8e836b88bc2bdd154
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2016-06-15 09:58:36 +0100

    player snap

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -31,24 +31,29 @@ public class Player :WorldObject
 	void Update () 
     {
         Movement();
-        Tile tile = TileManager.instance.GetTile(transform.position);
-        if (tile)
-        {
-            tile.GetComponentInChildren<SpriteRenderer>().color = Color.blue;
-        }
+        MakeItBlue();
 	}
     void Movement()
     {
        if (Input.GetAxis("Horizontal")!=0)
        {
-           transform.position = new Vector2(transform.position.x + Input.GetAxis("Horizontal"),transform.position.y);
+           transform.position = TileManager.instance.GetTile(new Vector2((transform.position.x+Input.GetAxis("Horizontal")),transform.position.y)).transform.position;
            angle = Input.GetAxis("Horizontal") > 0 ? 270 : 90;
        }
        else if (Input.GetAxis("Vertical")!=0)
        {
-           transform.position = new Vector2(transform.position.x, transform.position.y + Input.GetAxis("Vertical"));
+           transform.position = TileManager.instance.GetTile(new Vector2(transform.position.x , (transform.position.y + Input.GetAxis("Vertical")))).transform.position;
+
            angle = Input.GetAxis("Vertical") > 0 ? 0 : 180;
        }
        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+    void MakeItBlue()
+    {
+        Tile tile = TileManager.instance.GetTile(transform.position);
+        if (tile)
+        {
+            tile.GetComponentInChildren<SpriteRenderer>().color = Color.blue;
+        }
     }
 }
