0f704176277c02e52398781fc7bd4c997fa620bf
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2016-06-16 16:34:20 +0100

    cubicle scripts

---
+++ Assets/scripts/Cubicle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cubicle : WorldObject 
+{
+    public SpriteRenderer deskFodder;
+    public Sprite[] tidyDesk;
+    public Sprite[] messyDesk;
+    int currentDesk;
+	// Use this for initialization
+	void Awake () 
+    {
+        currentDesk = Random.Range(0, tidyDesk.Length);
+        deskFodder.sprite = tidyDesk[currentDesk];
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+}
