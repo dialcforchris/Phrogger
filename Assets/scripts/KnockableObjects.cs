64ad6a4482945ddff340de20850654b46e1fbb03
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2016-06-20 17:13:35 +0100

    knocking over objects

---
+++ Assets/scripts/KnockableObjects.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnockableObjects : Wall
+{
+    public Sprite altImage;
+	
+    public void Knocked()
+    {
+        spriteRenderer.sprite = altImage;
+    }
+    public override bool CheckMovement(WorldObject _obj)
+    {
+        Knocked();
+        return base.CheckMovement(_obj);
+
+    }
+}
