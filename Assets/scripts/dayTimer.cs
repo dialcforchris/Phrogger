50f293e98a5168c035b4d28d9ba889da92e081fc
Craig <ctinney94@gmail.com>
2016-06-23 11:58:47 +0100

    Day transitions! GAME LOOP IS FULLY DONE BOYS FUCKIN' WOO!

--- Assets/scripts/dayTimer.cs
+++ Assets/scripts/dayTimer.cs
@@ -36,26 +36,6 @@ public class dayTimer : MonoBehaviour {
 
     public void NewDay()
     {
-        currentTime = 0;
-        progressUI.SetActive(false);
-        dayFinishedText.Stop();
-        progressUI.GetComponent<Image>().enabled = false;
-        dayCompletedHeader.text = string.Empty;
-        dayCompletedHeader.enabled = false;
-        filedText.enabled = false;
-        for (int i = 0; i < todaysEmails.Count; i++)
-        {
-            Destroy(emailObjects[i]);
-        }
-        todaysEmails.Clear();
-        emailObjects.Clear();
-
-        performanceText.enabled = false;
-        performanceResult.text = string.Empty;
-        finishedDisplay = false;
-
-        WorkerManager.instance.SetupDefaultPositions();
-
         transitioning = false;
         GameStateManager.instance.ChangeState(GameStates.STATE_GAMEPLAY);
     }
@@ -97,14 +77,67 @@ public class dayTimer : MonoBehaviour {
     IEnumerator NextDayTransition()
     {
         //Fade to black
-        //Fade in day text
+        while(background.color.a < 1)
+        {
+            Color col = background.color;
+            col.a += Time.deltaTime;
+            background.color = col;
+            yield return new WaitForEndOfFrame();
+        }
 
-        yield return new WaitForEndOfFrame();
+        DayText.text = "Day " + (StatTracker.instance.numOfDaysCompleted +1);
+
+        //Fade in day text
+        while (DayText.color.a < 1)
+        {
+            Color col = DayText.color;
+            col.a += Time.deltaTime;
+            DayText.color = col;
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return new WaitForSeconds(1.5f);
 
         foreach (WorldObject _wo in FindObjectsOfType<WorldObject>())
         {
             _wo.Reset();
         }
+        currentTime = 0;
+        progressUI.SetActive(false);
+        dayFinishedText.Stop();
+        progressUI.GetComponent<Image>().enabled = false;
+        dayCompletedHeader.text = string.Empty;
+        dayCompletedHeader.enabled = false;
+        filedText.enabled = false;
+        for (int i = 0; i < todaysEmails.Count; i++)
+        {
+            Destroy(emailObjects[i]);
+        }
+        todaysEmails.Clear();
+        emailObjects.Clear();
+
+        performanceText.enabled = false;
+        performanceResult.text = string.Empty;
+        finishedDisplay = false;
+
+        //Also reset the clock
+        smallHand.rectTransform.rotation = Quaternion.Euler(0, 0, 90);
+        bigHand.rectTransform.rotation = Quaternion.Euler(0, 0, 0);
+
+        WorkerManager.instance.SetupDefaultPositions();
+
+        //Fade out both
+        while (DayText.color.a > 0)
+        {
+            Color colA = DayText.color;
+            colA.a -= Time.deltaTime;
+            DayText.color = colA;
+            Color colB = background.color;
+            colB.a -= Time.deltaTime;
+            background.color = colB;
+            yield return new WaitForEndOfFrame();
+        }
+
         NewDay();
     }
 
@@ -155,24 +188,32 @@ public class dayTimer : MonoBehaviour {
 
         performanceText.enabled = true;
         yield return new WaitForSeconds(2f);
-        
-        if (correct / todaysEmails.Count < .15f)
-            performanceResult.text = "HOW DO YOU STILL HAVE A JOB??";
-        else if (correct / todaysEmails.Count < .30f)
-            performanceResult.text = "SHAME";
-        else if (correct / todaysEmails.Count < .45f)
-            performanceResult.text = "NEEDS IMPROVEMENT";
-        else if (correct / todaysEmails.Count < .60f)
-            performanceResult.text = "MEDIOCRE";
-        else if (correct / todaysEmails.Count < .75f)
-            performanceResult.text = "SUPRISINGLY GOOD";
-        else if (correct / todaysEmails.Count < .90f)
-            performanceResult.text = "RIBBITING!";
+
+        if (todaysEmails.Count == 0)
+        {
+            performanceResult.text = "USELESS";
+            performanceResult.color = Color.red;
+        }
         else
-            performanceResult.text = "EMPLOYEE OF THE DAY!";
+        {
 
-        performanceResult.color = Color.Lerp(Color.red, Color.green, correct / todaysEmails.Count);
+            if (correct / todaysEmails.Count < .15f)
+                performanceResult.text = "HOW DO YOU STILL HAVE A JOB??";
+            else if (correct / todaysEmails.Count < .30f)
+                performanceResult.text = "SHAME";
+            else if (correct / todaysEmails.Count < .45f)
+                performanceResult.text = "NEEDS IMPROVEMENT";
+            else if (correct / todaysEmails.Count < .60f)
+                performanceResult.text = "MEDIOCRE";
+            else if (correct / todaysEmails.Count < .75f)
+                performanceResult.text = "SUPRISINGLY GOOD";
+            else if (correct / todaysEmails.Count < .90f)
+                performanceResult.text = "RIBBITING!";
+            else
+                performanceResult.text = "EMPLOYEE OF THE DAY!";
 
+            performanceResult.color = Color.Lerp(Color.red, Color.green, correct / todaysEmails.Count);
+        }
         performanceResult.enabled = true;
 
         finishedDisplay = true;
