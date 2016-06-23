1bb85961f3ac09abb30c8393590e3cc327373e4c
Shaun Landy <Shaun2.Landy@live.uwe.ac.uk>
2016-06-23 16:16:05 +0100

    CorpseFix

--- Assets/scripts/FrogCorpse.cs
+++ Assets/scripts/FrogCorpse.cs
@@ -7,7 +7,7 @@ public class FrogCorpse : WorldObject
 
     public override void Interaction(WorldObject _obj)
     {
-        if (_obj.tag == "Worker" || _obj.tag == "Boss")
+        if (_obj.tag == "Worker")
         {
             if (((Worker)_obj).isJanitor)
             {
