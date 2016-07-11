9a8ee8f941c3114d66e24d207bceb3d54d04ca71
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2016-07-11 12:11:10 +0100

    enter name input now better

--- Assets/scripts/ScoreBoard/EnterName.cs
+++ Assets/scripts/ScoreBoard/EnterName.cs
@@ -14,7 +14,7 @@ public class EnterName : MonoBehaviour
     float maxCool = 0.2f;
     string theName = string.Empty;
     bool finished;
-
+  
 	void Start () 
     {
         coolDown = maxCool;
@@ -38,62 +38,68 @@ public class EnterName : MonoBehaviour
 
     void MenuInput()
     {
-        if (Input.GetAxis("Horizontal") != 0&&SelectCoolDown())
+        if (ConvertToPos("Horizontal", "Vertical"))
         {
-            if (Input.GetAxis("Horizontal") > 0)
+            if (Input.GetAxis("Horizontal") != 0 && SelectCoolDown())
             {
-                if (selectBox == box.Length-1)
+                if (Input.GetAxis("Horizontal") > 0)
                 {
-                    selectBox = 0;
+                    if (selectBox == box.Length - 1)
+                    {
+                        selectBox = 0;
+                    }
+                    else
+                    {
+                        selectBox++;
+                    }
                 }
-                else
+                else if (Input.GetAxis("Horizontal") < 0)
                 {
-                    selectBox++;
+                    if (selectBox == 0)
+                    {
+                        selectBox = box.Length - 1;
+                    }
+                    else
+                    {
+                        selectBox--;
+                    }
                 }
+                selectChar = currentCharacter[selectBox];
+                coolDown = 0;
             }
-            else if(Input.GetAxis("Horizontal")<0)
-            {
-                if (selectBox ==0)
-                {
-                    selectBox = box.Length-1;
-                }
-                else
-                {
-                    selectBox--;
-                }
-            }
-            selectChar = currentCharacter[selectBox];
-            coolDown = 0;
         }
-        else if (Input.GetAxis("Vertical") != 0 && SelectCoolDown())
+        else
         {
-            if (Input.GetAxis("Vertical")<0)
+            if (Input.GetAxis("Vertical") != 0 && SelectCoolDown())
             {
-                if (selectChar > 65)
-                    selectChar--;
+                if (Input.GetAxis("Vertical") < 0)
+                {
+                    if (selectChar > 65)
+                        selectChar--;
 
-                else
-                    selectChar = 90;
-            }
-            else if (Input.GetAxis("Vertical")>0)
-            {
-                if (selectChar < 90)
-                    selectChar++;
+                    else
+                        selectChar = 90;
+                }
+                else if (Input.GetAxis("Vertical") > 0)
+                {
+                    if (selectChar < 90)
+                        selectChar++;
 
-                else
-                    selectChar = 65;
+                    else
+                        selectChar = 65;
+                }
+                //if (Input.GetAxis("Vertical") > 0 && selectChar < 90)
+                //if (selectChar == 90)
+                //{
+                //    selectChar = 65;
+                //}
+                //else
+                //{
+                //    selectChar++;
+                //}
+                coolDown = 0;
+                currentCharacter[selectBox] = selectChar;
             }
-            //if (Input.GetAxis("Vertical") > 0 && selectChar < 90)
-            //if (selectChar == 90)
-            //{
-            //    selectChar = 65;
-            //}
-            //else
-            //{
-            //    selectChar++;
-            //}
-            coolDown = 0;
-            currentCharacter[selectBox] = selectChar;
         }
     }
 
@@ -140,5 +146,26 @@ public class EnterName : MonoBehaviour
         }
         else
             StartCoroutine(MainMenu.instance.wholeScreenFade(true));
+    }
+    bool ConvertToPos(string ho, string ve)
+    {
+        float hori = Input.GetAxis(ho);
+        float verti = Input.GetAxis(ve);
+        if (hori < 0)
+        {
+            hori -= hori * 2;
+        }
+        if (verti < 0)
+        {
+            verti -= verti * 2;
+        }
+        if (hori>verti)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
